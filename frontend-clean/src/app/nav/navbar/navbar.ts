f505ea842b5811b9541c9a98754d300a9f743dcf
javier huaman <javierantonio_huamannunez@iescarlesvallbona.cat>
2026-04-11 01:28:08 +0200

    fix

--- frontend-clean/src/app/nav/navbar/navbar.ts
+++ frontend-clean/src/app/nav/navbar/navbar.ts
@@ -20,13 +20,15 @@ ngOnInit() {
   this.cargarUsuario();
 
   const tema = localStorage.getItem('tema') || 'light';
-  document.body.className = tema;
+
+  // 🔥 aplicar tema correctamente
+  document.body.classList.remove('light', 'dark');
+  document.body.classList.add(tema);
 
   this.router.events.subscribe(() => {
     this.cargarUsuario();
   });
 }
-
   cargarUsuario() {
     this.nombre = localStorage.getItem('nombre');
     this.rol = localStorage.getItem('rol');
