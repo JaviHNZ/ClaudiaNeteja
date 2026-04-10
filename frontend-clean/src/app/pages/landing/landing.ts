4fecbbd435e1e8be6982d6eecabce9355e3ae2f4
javier huaman <javierantonio_huamannunez@iescarlesvallbona.cat>
2026-04-10 22:48:42 +0200

    fix router landing

--- frontend-clean/src/app/pages/landing/landing.ts
+++ frontend-clean/src/app/pages/landing/landing.ts
@@ -4,8 +4,8 @@ import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-landing',
   standalone: true,
+  imports: [RouterModule], // 🔥 ESTO FALTABA
   templateUrl: './landing.html',
   styleUrls: ['./landing.css'],
-  imports: [RouterModule],
 })
 export class Landing {}
