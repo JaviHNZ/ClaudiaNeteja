506c47df48c0a0847ad32b283187728c6c593b4f
javier huaman <javierantonio_huamannunez@iescarlesvallbona.cat>
2026-04-11 01:10:38 +0200

    page admin boopstrap

--- frontend-clean/src/app/pages/admin/admin.ts
+++ frontend-clean/src/app/pages/admin/admin.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../../services/api';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ChangeDetectorRef } from '@angular/core';
+
 @Component({
   selector: 'app-admin',
   standalone: true,
@@ -14,32 +15,33 @@ export class Admin implements OnInit {
   trabajadores: any[] = [];
 
   constructor(
-  private api: ApiService,
-  private router: Router,
-  private cd: ChangeDetectorRef
-) {}
-ngOnInit() {
-  if (localStorage.getItem('rol') !== 'admin') {
-    this.router.navigate(['/registros']);
-    return;
+    private api: ApiService,
+    private router: Router,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    if (localStorage.getItem('rol') !== 'admin') {
+      this.router.navigate(['/registros']);
+      return;
+    }
+
+    this.api.getResumenTrabajadores().subscribe({
+      next: (res: any) => {
+        this.trabajadores = res;
+        this.cd.detectChanges();
+      },
+      error: (err) => console.error(err),
+    });
   }
 
- this.api.getResumenTrabajadores().subscribe({
-  next: (res: any) => {
-    this.trabajadores = res;
+  verDetalle(trabajador: any) {
+    this.router.navigate(['/admin/usuario', trabajador.id]);
+  }
 
-    this.cd.detectChanges(); // 🔥 FIX
-  },
-  error: (err) => console.error(err),
-});
-}
-verDetalle(trabajador: any) {
-  this.router.navigate(['/admin/usuario', trabajador.id]);
-}
-formatearHoras(h: number) {
-  const horas = Math.floor(h);
-  const minutos = Math.round((h - horas) * 60);
-
-  return `${horas}h ${minutos}min`;
-}
+  formatearHoras(h: number) {
+    const horas = Math.floor(h);
+    const minutos = Math.round((h - horas) * 60);
+    return `${horas}h ${minutos}min`;
+  }
 }
